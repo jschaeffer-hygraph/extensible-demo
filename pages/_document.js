246bd1814b3b1bfef50c96be3eedfca55bd6a882
Jörg Schäffer <joerg.schaeffer@hygraph.com>
2025-08-24 22:52:48 +0200

    added VWO and Google Tag for GA4

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,15 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <VWOScript accountId="1118466" />
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-VVQ7079CWY"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-VVQ7079CWY');
+          </script>
           <link
             rel="stylesheet"
             href="https://unpkg.com/@tailwindcss/typography@0.2.x/dist/typography.min.css"
