b729b6ba92a261c08385d736e0b4b5f2d6dc4b9d
Jörg Schäffer <joerg.schaeffer@hygraph.com>
2025-09-04 17:32:46 +0200

    Fix JSX script syntax and clear cache

--- pages/_document.js
+++ pages/_document.js
@@ -14,13 +14,16 @@ export default class MyDocument extends Document {
           <VWOScript accountId="1118466" />
           {/* Google tag (gtag.js) */}
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-VVQ7079CWY"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-VVQ7079CWY');
-          </script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-VVQ7079CWY');
+              `,
+            }}
+          />
           <link
             rel="stylesheet"
             href="https://unpkg.com/@tailwindcss/typography@0.2.x/dist/typography.min.css"
